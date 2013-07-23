885ef80d83d1759f6d57d3910c47898cde61500d
PaulWillshire <paulwillshire@gmail.com>
2013-07-23 11:03:39 +0100

    Int validation method
    created new method to handle validation of ints

---
+++ ThirdCarProject/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdCarProject
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int maxSpeed;
+
+        public Car(string make, string model, int maxSpeed)
+        {
+            this.make = make;
+            this.model = model;
+            this.maxSpeed = maxSpeed;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set { maxSpeed = value; }
+        }
+
+        public void PrintCar()
+        {
+            Console.WriteLine("Make: " + make);
+            Console.WriteLine("Model: " + model);
+            Console.WriteLine("Max Speed: " + maxSpeed + "mph");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ThirdCarProject/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdCarProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = ReadInt("How many cars would you like to create?");
+
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string make = string.Empty;
+                bool makeIsValid = false;
+                while (makeIsValid == false)
+                {
+                    Console.WriteLine("Please enter the make of your car (Must be 10 characters or less):");
+                    make = Console.ReadLine();
+
+                    if (make.Length > 10 || string.IsNullOrEmpty(make))
+                    {
+                        makeIsValid = false;
+                    }
+                    else
+                    {
+                        makeIsValid = true;
+                    }
+                }
+
+                string model = string.Empty;
+                bool modelIsValid = false;
+                while (modelIsValid == false)
+                {
+                    Console.WriteLine("Please enter the model of your car (Must be 15 characters or less):");
+                    model = Console.ReadLine();
+
+                    if (model.Length > 15 || string.IsNullOrEmpty(model))
+                    {
+                        modelIsValid = false;
+                    }
+                    else
+                    {
+                        modelIsValid = true;
+                    }
+                }
+
+                int maxSpeed = ReadInt("Please enter the Maximum speed of your car:", 0, 200);
+                
+                Car car1 = new Car(make, model, maxSpeed);
+                car1.PrintCar();
+
+            }
+        }
+
+        private static int ReadInt(string message, int minValue = int.MinValue, int maxValue = int.MaxValue)
+        {
+            int enteredValue = 0;
+            string enteredString = string.Empty;
+
+            while (true)
+            {
+                Console.WriteLine(message);
+                enteredString = Console.ReadLine();
+
+                if (int.TryParse(enteredString, out enteredValue))
+                {
+                    if (enteredValue > minValue && enteredValue < maxValue)
+                    {
+                        return enteredValue;
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
